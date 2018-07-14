9339584296c7e1a1b47fde5cd17956673645732b
Hammed Noibi <hnobi08@yahoo.com>
2018-07-14 23:43:25 +0100

    create modifyEntry controller,route and middleware

--- server/controllers/entry.js
+++ server/controllers/entry.js
@@ -31,17 +31,37 @@ export default class EntryController {
       date,
       entry
     });
-    return res.status(200)
+    return res.status(201)
       .json({
         status: 'Success',
         message: 'Successfully added new entry',
         entryData
       });
-
-
   }
 
-
+  static modifyEntry(req, res) {
+    const {
+      title, date, entry
+    } = req.body;
+    for (let i = 0; i < entryData.length; i += 1) {
+      if (entryData[i].id === parseInt(req.params.entryId, 10)) {
+        entryData[i].title = (title) || entryData[i].title;
+        entryData[i].date = (date) || entryData[i].date;
+        entryData[i].entry = (entry) || entryData[i].entry;
+        return res.status(200)
+          .json({
+            status: 'Success',
+            message: 'Successfully updated  your entry',
+            entryData,
+          });
+      }
+    }
+    res.status(404);
+    res.json({
+      status: 'Failed',
+      message: 'entry id does not exist',
+    });
+  }
 
 
 
