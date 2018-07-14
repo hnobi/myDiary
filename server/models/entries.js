7c03b6ed121ad402a23d9af2029c4e23bc3c3494
Hammed Noibi <hnobi08@yahoo.com>
2018-07-14 12:53:01 +0100

    create entries model

---
+++ server/models/entries.js
@@ -0,0 +1,20 @@
+const Entries = [
+  {
+    id: 1,
+    title: 'the great day',
+    date: '27-05-2018',
+    entry: 'Reflection describes a real or imaginary scene, event, interaction, passing thought, memory'
+  },
+  {
+    id: 2,
+    title: 'black strive',
+    date: '07-15-2018',
+    entry: 'Reflection describes a real or imaginary scene, event, interaction, passing thought, memory'
+  },
+  {
+    id: 3,
+    title: 'panther party',
+    date: '13-22-2017',
+    entry: 'Reflection describes a real or imaginary scene, event, interaction, passing thought, memory'
+  }
+]
