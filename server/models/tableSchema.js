8fad1e0359470b2c831aadbb0914fbd38a04b284
Hammed Noibi <hnobi08@yahoo.com>
2018-08-27 16:42:12 +0100

    feat(Reminder seting): modify the  implementation  mail reminder settetting controller :

--- server/models/tableSchema.js
+++ server/models/tableSchema.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
 const client = new Client(connectionString);
 
 client.connect();
