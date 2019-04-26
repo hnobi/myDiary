ca8dae53ec6da9b703402be220312248a3e025c1
Hammed Noibi <hnobi08@yahoo.com>
2019-04-26 18:55:31 +0100

    Revert "fix-vulnerability"

--- server/models/db.js
+++ server/models/db.js
@@ -7,10 +7,12 @@ const config = configuration[env];
 
 const connectionString = config.url;
 dotenv.config();
-
+// const connectionString = process.env.DATABASE_URL;
 const db = new Pool({ connectionString });
 db.connect().then(() => {
   console.log(' succefully connected to postgresDB');
+  console.log(connectionString);
+
 }).catch((err) => {
   console.log(err.message);
 });
